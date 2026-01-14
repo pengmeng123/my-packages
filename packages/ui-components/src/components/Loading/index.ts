4c999e88fa771bd073e185cb6c8ad75721d08491
pengmeng <pengmeng@qcc.com>
2026-01-14 18:24:48 +0800

    kk

--- packages/ui-components/src/components/Loading/index.ts
+++
@@ -1,9 +0,0 @@
-import { VueConstructor } from "vue";
-
-import Loading from "./Loading";
-
-(Loading as any).install = function (Vue: VueConstructor): void {
-  Vue.component("PLoading", Loading);
-};
-
-export default Loading;
